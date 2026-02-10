2ac951f1d2f869f1661352cd9f2080c3f9c63e4f
Raghunath Mamidala <raghunathmamidala12345@gmail.com>
2026-02-10 21:11:20 +0530

    Singleton

---
+++ DesignPattern/Creational/Singleton/singleton.py
@@ -0,0 +1,18 @@
+import threading
+
+
+class Singleton:
+    _instance = None
+    _lock = threading.Lock()
+
+    def __new__(cls):
+        if cls._instance is None:
+            with cls._lock:
+                if cls._instance is None:
+                    cls._instance = super().__new__(cls)
+
+        return cls._instance
+
+a = Singleton()
+b = Singleton()
+print(a is b)
